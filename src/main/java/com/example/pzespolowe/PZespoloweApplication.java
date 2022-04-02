b527101920658d74f4f25787e0d3ce266fb02d5f
okocjan <kocjan.oskar@gmail.com>
2022-04-02 13:24:28 +0200

    spring added

---
+++ src/main/java/com/example/pzespolowe/PZespoloweApplication.java
@@ -0,0 +1,13 @@
+package com.example.pzespolowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PZespoloweApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PZespoloweApplication.class, args);
+    }
+
+}
