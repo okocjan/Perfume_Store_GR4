57bcc9e7fd1ee065cf326eea869669991f08042e
lkiedos <93326006+lkiedos@users.noreply.github.com>
2022-05-14 21:07:18 +0200

    Revert "Lkiedos dev"

--- src/main/java/com/example/pzespolowe/Repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.pzespolowe.Repositories;
-
-import com.example.pzespolowe.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
--- src/main/java/com/example/pzespolowe/Repositories/ZamowienieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.pzespolowe.Repositories;
-
-import com.example.pzespolowe.Models.Zamowienie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ZamowienieRepository extends JpaRepository<Zamowienie, Integer> {
-}
